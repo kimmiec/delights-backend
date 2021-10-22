4125dbc35d025f7827e9cd07287136ea927dfe68
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-10-21 20:48:10 -0400

    route name change

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const { PORT = 4000, MONGODB_URL } = process.env;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors')
-const morgan = require('morgan')
+const cors = require('cors');
+const morgan = require('morgan');
 
 // ============ CONNECTIONS ============
 mongoose.connect(MONGODB_URL, {
@@ -23,9 +23,9 @@ const BlogSchema = new mongoose.Schema({
     name: String,
     image: String,
     desc: String
-})
+});
 
-const Blog = mongoose.model('Blog', BlogSchema)
+const Blog = mongoose.model('Blog', BlogSchema);
 
 // ============ MIDDLEWARE ============
 app.use(cors());
@@ -33,15 +33,17 @@ app.use(morgan('dev'));
 app.use(express.json());
 // ============ ROUTES ============
 // index
-app.get('/', (req,res) =>{
-    // res.send('hello pikachu')
+app.get('/', async (req,res) =>{
+    res.send('hello pikachu')
+})
+
+app.get('/home', async (req,res) =>{
     try {
         res.json(await Blog.find({}))
     } catch (error) {
         res.status(400).json(error)
     }
 })
-
 // create
 app.post('/post', async (req,res) =>{
     try{
